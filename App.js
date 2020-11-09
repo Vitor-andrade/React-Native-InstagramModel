c29518f1536cc574cf7f7c8a01cf987d818e513f
Vitor de Andrade Cavalcante <vitorandrade9@hotmail.com>
2020-11-09 14:06:09 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import {ScrollView, FlatList} from "react-native";
+import { Cabecalho } from './src/Components/Cabecalho';
+import { Foto } from './src/Components/Foto';
+import lerFotos from './src/api/feed';
+import {Comentarios} from './src/Components/Comentarios';
+
+
+const App: () => React$Node = () => {
+  const [fotos, setFotos] = useState([]);
+
+  useEffect(() => {
+    
+    lerFotos(setFotos);
+  },[])
+ 
+  return (
+    <ScrollView>
+      <FlatList
+        data={fotos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item})=> 
+        <Fragment> 
+          <Cabecalho nomeUsuario={item.userName} 
+          urlImage={item.userURL}/>
+          <Foto urlFoto={item.url}
+          descricao={item.description}
+          qntLikes={item.likes}
+          />
+          <Comentarios comentarios={item.comentarios}/>
+        </Fragment>}
+      />
+
+    </ScrollView>
+  )
+};
+
+export default App;
